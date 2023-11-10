82eb1660e840968b04705df39f152c1570f5d824
Francisco Zuidwijk <electrocisko@gmail.com>
2023-11-10 18:25:48 -0300

    bootstrap

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import express from 'express';
-import config from './config.js'
-import db from './database/postgre.js';
-
+import express from "express";
+import config from "./config.js";
+import db from "./database/postgre.js";
 
 const PORT = config.app.PORT;
 
@@ -10,16 +9,22 @@ db.connect();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.get('/', (req,res) => {
-
- return res.render('index.ejs')
-})
-
+app.get("/", async (req, res) => {
+  try {
+    const data = await db.query("SELECT * FROM book");
+    const books = data.rows;
+    return res.render("index.ejs",{books});
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "Error in get index",
+    });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server listening http://localhost:${PORT}`);
-})
+  console.log(`Server listening http://localhost:${PORT}`);
+});
